f657ca1f990dcc6bc84278f99b0294ec4a59768a
Naimish <your-email@gmail.com>
2026-08-21 14:46:53 +0530

    Add health check API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.api.routes.health import router as health_router
 from app.core.config import APP_NAME, ENVIRONMENT
 
 app = FastAPI(title=APP_NAME)
@@ -11,3 +12,6 @@ def home():
         "message": "StudyMind backend is running!",
         "environment": ENVIRONMENT
     }
+
+
+app.include_router(health_router)
